d275686b06c9e3226bde1020c70cc196b15e16b0
Pierre Grandin <grandinp@gmail.com>
2019-01-08 09:07:31 -0700

    Initial code import

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM ubuntu
+RUN apt-get update && apt-get install -y python-pip
+RUN pip install platformio
+RUN apt-get install -y git
+ARG CACHE_DATE=not_a_date
+ADD . /sbase
+RUN cd /sbase/Marlin && platformio run -e LPC1768
+RUN md5sum /sbase/Marlin/.pioenvs/LPC1768/firmware.bin
---
+++ build.sh
@@ -0,0 +1,6 @@
+docker build --build-arg CACHE_DATE=$(date +"%m/%d-%H:%M") -t marlin .
+docker run -ti --name marlin marlin md5sum /sbase/Marlin/.pioenvs/LPC1768/firmware.bin
+docker cp marlin:/sbase/Marlin/.pioenvs/LPC1768/firmware.bin .
+docker rm marlin
+md5sum firmware.bin
+scp firmware.bin octoprint:
