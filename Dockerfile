c878ad8cf235e1a9a6897bb2a0e65e0a50a34fec
Pierre Grandin <grandinp@gmail.com>
2020-11-16 16:13:20 -0700

    Updated PIO paths

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu
+FROM ubuntu:18.04
 RUN apt-get update && apt-get install -y python-pip
 RUN pip install platformio
 RUN apt-get install -y git
@@ -6,4 +6,4 @@ ARG CACHE_DATE=not_a_date
 ADD . /sbase
 RUN cd /sbase/Marlin && patch -p0 < ../sbase.patch
 RUN cd /sbase/Marlin && platformio run -e LPC1768
-RUN md5sum /sbase/Marlin/.pioenvs/LPC1768/firmware.bin
+RUN md5sum /sbase/Marlin/.pio/build/LPC1768/firmware.bin
--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 docker build --build-arg CACHE_DATE=$(date +"%m/%d-%H:%M") -t marlin .
-docker run -ti --name marlin marlin md5sum /sbase/Marlin/.pioenvs/LPC1768/firmware.bin
-docker cp marlin:/sbase/Marlin/.pioenvs/LPC1768/firmware.bin .
-docker rm marlin
+docker run -ti --name marlin marlin md5sum /sbase/Marlin/.pio/build/LPC1768/firmware.bin
+docker cp marlin:/sbase/Marlin/.pio/build/LPC1768/firmware.bin . && docker rm marlin
 mv firmware.bin `git rev-parse HEAD`.bin
