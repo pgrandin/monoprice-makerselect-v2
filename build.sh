7f49a62ddd6bb91182d2d188f9d12b266b68d599
Pierre Grandin <grandinp@gmail.com>
2024-12-17 10:01:40 -0700

    docker based build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker build --build-arg CACHE_DATE=$(date +"%m/%d-%H:%M") -t marlin .
-docker run -ti --name marlin marlin md5sum /sbase/Marlin/.pio/build/LPC1768/firmware.bin
-docker cp marlin:/sbase/Marlin/.pio/build/LPC1768/firmware.bin . && docker rm marlin
+docker run -ti --name marlin marlin md5sum /Marlin/.pio/build/LPC1768/firmware.bin
+docker cp marlin:/Marlin/.pio/build/LPC1768/firmware.bin . && docker rm marlin
 mv firmware.bin `git rev-parse HEAD`.bin
