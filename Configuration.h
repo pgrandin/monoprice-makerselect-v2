8b5a64d5d3bfd06207fb737e59a3094593fe5599
pgrandin <pgrandin@users.noreply.github.com>
2025-09-26 16:20:31 -0600

    Fix probe X offset to -40mm to prevent probing outside bed

--- Configuration.h
+++ Configuration.h
@@ -1511,7 +1511,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -24, -36, -3.7 }
+#define NOZZLE_TO_PROBE_OFFSET { -40, -36, -3.7 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
