8731037755dda00f093d7d04489e70ac24449caa
pgrandin <pgrandin@users.noreply.github.com>
2025-09-26 16:22:16 -0600

    Fix bed offset: Set X_MIN_POS to 0 (was -15) to correct probing area

--- Configuration.h
+++ Configuration.h
@@ -1511,7 +1511,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -40, -36, -3.7 }
+#define NOZZLE_TO_PROBE_OFFSET { -24, -36, -3.7 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1728,7 +1728,7 @@
 #define Y_BED_SIZE 205
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS -15
+#define X_MIN_POS 0
 #define Y_MIN_POS -8
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
