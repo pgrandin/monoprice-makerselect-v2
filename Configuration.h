7d7eff9a99e8a23825959b0481124f532179ba73
pgrandin <pgrandin@users.noreply.github.com>
2025-09-26 13:32:32 -0600

    feat(printer): cap z height to 189

--- Configuration.h
+++ Configuration.h
@@ -1408,7 +1408,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 200
+#define Z_MAX_POS 189
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
